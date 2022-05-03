50e1335e37b14de293e6d6d226e5307bfa3a6fcf
WaterySaucer113pp <93452656+bbaj8@users.noreply.github.com>
2022-05-03 18:27:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/AihamSharaan/Hanon.git")
+os.system("git clone https://github.com/bbaj8/Fscsfdeexdss3.git")
 os.system("Fscsfdeexdss3 && python bot.py")
